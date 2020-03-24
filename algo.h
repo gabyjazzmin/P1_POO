735b41f20174d19a8d723bd0ce3fcf5d924d956c
gabyjazzmin <61790728+gabyjazzmin@users.noreply.github.com>
2020-03-24 17:18:22 -0600

    corrección con cualquier varible
    Aquí se corrigió el poder meter cualquier variable

--- algo.h
+++ algo.h
@@ -1,120 +1,156 @@
 #ifndef algo_h
 #define algo_h
-
+#include <string.h>
 #include <cmath>
+using namespace std;
 
 class algo {
     private:
-        string sNum1;
-        char cOperador;
-        string sNum2;
+        string sDefinition;
+        /*string sNum1;
+        string cOperador;
+        string sNum2;*/
+        string numeroEnClave;
 
         //length of string
         int iNum1;
         int iNum2;
 
     public:
-        algo(string sN1, char cO, string sN2);
+        algo(string _numeroEnClave, string _sDefinition);
 
         double convertir();
 };
 
-algo::algo(string sN1, char cO, string sN2) {
+/*algo::algo(string sN1, string cO, string sN2) {
     sNum1 = sN1;
     cOperador = cO;
     sNum2 = sN2;
 
     iNum1 = 0;
     iNum2 = 0;
+}*/
+algo::algo(string _numeroEnClave, string _sDefinition) {
+    numeroEnClave=_numeroEnClave;
+    sDefinition=_sDefinition;
 }
 
-double algo::convertir() {
-    for (int i = 0 ; i < sNum1.length() ; ++i) {
-        switch (sNum1[i]) {
-            case 'B':
-                    iNum1 += 0;
-                break;
-            case 'y':
-                    iNum1 += 1 * pow(10, sNum1.length() - i - 1);
-                break;
-            case '/':
-                    iNum1 += 2 * pow(10, sNum1.length() - i - 1);
-                break;
-            case '8':
-                    iNum1 += 3 * pow(10, sNum1.length() - i - 1);
-                break;
-            case 'T':
-                    iNum1 += 4 * pow(10, sNum1.length() - i - 1);
-                break;
-            case '&':
-                    iNum1 += 5 * pow(10, sNum1.length() - i - 1);
-                break;
-            case 'Q':
-                    iNum1 += 6 * pow(10, sNum1.length() - i - 1);
-                break;
-            case 'E':
-                    iNum1 += 7 * pow(10, sNum1.length() - i - 1);
-                break;
-            case 'P':
-                    iNum1 += 8 * pow(10, sNum1.length() - i - 1);
-                break;
-            case 'F':
-                    iNum1 += 9 * pow(10, sNum1.length() - i - 1);
-                break;
-        }
-    }
+double algo:: convertir(){
+  /*int iNewsDefinition;
+   while(!entrada.eof()){
+   getline(entrada, sDefinition);
+    for(int i = 0; i<sDefinition.length(); i++){
+     //aqui se cambia la definicion de valores de string a enteros
+     iNewsDefinition[i] = stoi(sDefinition);
 
-    for (int i = 0 ; i < sNum2.length() ; ++i) {
-        switch (sNum2[i]) {
-            case 'B':
-                    iNum2 += 0;
-                break;
-            case 'y':
-                    iNum2 += 1 * pow(10, sNum2.length() - i - 1);
-                break;
-            case '/':
-                    iNum2 += 2 * pow(10, sNum2.length() - i - 1);
-                break;
-            case '8':
-                    iNum2 += 3 * pow(10, sNum2.length() - i - 1);
-                break;
-            case 'T':
-                    iNum2 += 4 * pow(10, sNum2.length() - i - 1);
-                break;
-            case '&':
-                    iNum2 += 5 * pow(10, sNum2.length() - i - 1);
-                break;
-            case 'Q':
-                    iNum2 += 6 * pow(10, sNum2.length() - i - 1);
-                break;
-            case 'E':
-                    iNum2 += 7 * pow(10, sNum2.length() - i - 1);
-                break;
-            case 'P':
-                    iNum2 += 8 * pow(10, sNum2.length() - i - 1);
-                break;
-            case 'F':
-                    iNum2 += 9 * pow(10, sNum2.length() - i - 1);
-                break;
-        }
-    }
 
-    switch (cOperador) {
-        case 'C':
-                return iNum1 + iNum2;
-            break;
-        case 'c':
-                return iNum1 - iNum2;
-            break;
-        case '#':
-                return iNum1 * iNum2;
-            break;
-        case '@':
-                return iNum1 / iNum2;
-            break;
-        default:
-                return -1;
-    }
+   }
+}  */
+  /*
+  int iNewSN1, iNewcO, iNewSN2;
+
+  iNewSN1 = stoi(sN1);
+  iNewCO = stoi(cO);
+  iNewSN2 = stoi()
+*/ 
+int num1, num2=0;
+string sNum1, sNum2; 
+char operacion;
+bool cambia2=false;
+for(int i=0; i<numeroEnClave.length(); i++){
+  if(sDefinition[0] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '0';
+      else
+        sNum2 += '0';
+  }
+  else if(sDefinition[1] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '1';
+      else
+        sNum2 += '1';
+  }
+  if(sDefinition[2] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '2';
+      else
+        sNum2 += '2';
+  }
+  else if(sDefinition[3] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '3';
+      else
+        sNum2 += '3';
+  }
+  if(sDefinition[4] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '4';
+      else
+        sNum2 += '4';
+  }
+  else if(sDefinition[5] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '5';
+      else
+        sNum2 += '5';
+  }
+  if(sDefinition[6] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '6';
+      else
+        sNum2 += '6';
+  }
+  if(sDefinition[7] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '7';
+      else
+        sNum2 += '7';
+  }
+  else if(sDefinition[8] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '8';
+      else
+        sNum2 += '8';
+  }
+   else if(sDefinition[9] == numeroEnClave[i]){
+    if(!cambia2)
+        sNum1 += '9';
+      else
+        sNum2 += '9';
+  }
+  if(sDefinition[10] == numeroEnClave[i]){
+    operacion='+';
+    cambia2=true;
+  }
+  else if(sDefinition[11] == numeroEnClave[i]){
+    operacion='-';
+    cambia2=true;
+  }
+  if(sDefinition[12] == numeroEnClave[i]){
+    operacion='*';
+    cambia2=true;
+  }
+  else if(sDefinition[13] == numeroEnClave[i]){
+    operacion='/';
+    cambia2=true;
+  }
+}
+
+switch(operacion){
+  case '+':
+  return stoi(sNum1)+stoi(sNum2);
+  break;
+  case '-':
+  return stoi(sNum1)-stoi(sNum2);
+  break;
+  case'*':
+  return stoi(sNum1)*stoi(sNum2);
+  break;
+  case '/':
+  return stoi(sNum1)/stoi(sNum2);
+  break;
+}
+return 0;
 }
 
 #endif
